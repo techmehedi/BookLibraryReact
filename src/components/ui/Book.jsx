5dc3b686ff584e8b2236245bac75043f650529ac
techmehedi <ecommehedi@gmail.com>
2023-06-04 23:57:01 -0400

    updates

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Price from "./Price";
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Book from '../components/ui/Book';
 import Price from '../components/ui/Price';
